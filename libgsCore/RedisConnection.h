eb660ec5e05de9cb64971bb7f6df78714883b553
WansangHan <sang5513@naver.com>
2018-05-11 00:44:04 +0900

    [한완상] public이 위로 가게 변경

--- libgsCore/RedisConnection.h
+++ libgsCore/RedisConnection.h
@@ -8,16 +8,17 @@ namespace Redis {
 
 class RedisConnection
 {
+public:
+  RedisConnection(const std::string& _address, const unsigned short _port);
+  ~RedisConnection();
+
+  bool Connect();
+private:
   boost::asio::ip::address address;
   const unsigned short port;
 
   boost::asio::io_service ioService;
   redisclient::RedisAsyncClient redis;
- public:
-  RedisConnection(const std::string& _address, const unsigned short _port);
-  ~RedisConnection();
-
-  bool Connect();
 };
 
 }
