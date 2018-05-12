f87f4b265d4132161ba28cc4a36c0e95d81f85d0
LoperDog <LoperDog10@gmail.com>
2018-05-12 17:59:28 +0900

    다중접속 테스트

--- Server/testmain.cpp
+++ Server/testmain.cpp
@@ -7,10 +7,24 @@ int main() {
   app::Property t(SERVER_IP, SERVER_PORT,true);
   app::Server First(t);
 
-  std::thread th([]() {
+  std::thread th1([]() {
     Sleep(3);
     std::cout << "add client;" << std::endl;
     
+    boost::shared_ptr<app::TCPClient> testClient =
+      boost::make_shared<app::TCPClient>();
+  });
+  std::thread th2([]() {
+    Sleep(3);
+    std::cout << "add client;" << std::endl;
+
+    boost::shared_ptr<app::TCPClient> testClient =
+      boost::make_shared<app::TCPClient>();
+  });
+  std::thread th3([]() {
+    Sleep(3);
+    std::cout << "add client;" << std::endl;
+
     boost::shared_ptr<app::TCPClient> testClient =
       boost::make_shared<app::TCPClient>();
   });
