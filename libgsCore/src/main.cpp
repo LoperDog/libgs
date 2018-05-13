f758349c775b826e43f5fc1492f29dd47f583970
Inkeun Kim <eastsideod@gmail.com>
2018-05-13 19:00:25 +0900

    main.cc 를 main.cpp 로 변경

---
+++ libgsCore/src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "redis/redis_connection.h"
+#include "redis/redis_parameter.h"
+
+
+int main() {
+  libgs::redis::InitializeRedisIoService();
+  libgs::redis::RedisConnection redisconnection;
+
+  redisconnection.Connect("127.0.0.1", 6379, [](
+      const bool success, const std::string &error_message) {
+    if (success) {
+      std::cout << "success" << std::endl;
+      return;
+    }
+    std::cout << "failed" << std::endl;
+  });
+
+  std::cout << "end connected";
+
+  while (1) {
+    std::cout << "wait.." << std::endl;
+    sleep(1);
+  }
+  return 0;
+}
