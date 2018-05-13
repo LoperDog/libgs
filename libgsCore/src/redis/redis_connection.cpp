61f98f8227bc35c79cf12ccd5ed99b5a62bb75c2
Inkeun Kim <eastsideod@gmail.com>
2018-05-13 19:01:58 +0900

    redis connection 에서 connect 함수 응답이 실제 redis connect 와 맞지 않는 문제 수정
    redisAsyncClient 를 사용했기 때문에 connect 함수의 응답은 실제로
    맞을 수도 있고 아닐 수 도있는 상태이다.
    이를 비동기 함수로 변경한다.
    
    추가로 unnamed namespace 에 io_service를 별도로 만든다.

--- libgsCore/src/redis/redis_connection.cpp
+++ libgsCore/src/redis/redis_connection.cpp
@@ -1,50 +1,82 @@
-#include "stdafx.h"
 #include "redis/redis_connection.h"
+
+#include "stdafx.h"
+
+#include <functional>
+#include <thread>
+
+#include <boost/assert.hpp>
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/ip/address.hpp>
+#include <redisclient/redisasyncclient.h>
+
 #include "redis/redis_parameter.h"
 
 
-namespace libgs {
-namespace Redis {
 
-struct RedisConnection::RedisConnectionImpl
-{
+namespace libgs {
+
+namespace redis {
+
+namespace {
+
+std::shared_ptr<boost::asio::io_service> the_io_service;
+std::shared_ptr<std::thread> the_thread;
+
+}  // unnamed namespace
+
+
+struct RedisConnection::RedisConnectionImpl {
+  RedisConnectionImpl()
+      : raw_redis_async_client(*the_io_service) {
+  }
+
   std::shared_ptr<RedisEndPoint> endpoint;
-  boost::asio::io_service ioService;
-  redisclient::RedisAsyncClient redis;
-  RedisConnectionImpl() : redis(ioService) {}
+  redisclient::RedisAsyncClient raw_redis_async_client;
 };
 
-RedisConnection::RedisConnection() :
-  pimpl_(std::make_shared<RedisConnection::RedisConnectionImpl>())
-{
+
+RedisConnection::RedisConnection()
+    : pimpl_(std::make_unique<
+          RedisConnection::RedisConnectionImpl>()) {
 }
 
-RedisConnection::~RedisConnection()
-{
+
+RedisConnection::~RedisConnection() {
 }
 
-bool RedisConnection::Connect(const std::string& address, const unsigned short port)
-{
+
+void RedisConnection::Connect(const std::string& address,
+                              const uint16_t port,
+                              const ConnectCallback &cb) {
   boost::system::error_code ec;
   boost::asio::ip::address::from_string(address, ec);
 
-  if (ec)
-  {
-    std::cout << "Invalid Address, errcode : " << ec << std::endl;
-    return false;
-  }
+  BOOST_ASSERT(ec == boost::system::errc::success);
 
-  pimpl_->endpoint = std::make_shared<RedisEndPoint>(address, port);
+  pimpl_->endpoint =
+      std::make_shared<RedisEndPoint>(address, port);
 
-  bool isSuccess = false;
-  pimpl_->redis.connect(pimpl_->endpoint->address, pimpl_->endpoint->port, [&isSuccess](bool _isSuccess, std::string errcode) {
-    isSuccess = _isSuccess;
-    std::cout << "Connect!" << std::endl;
-  });
-  pimpl_->ioService.run();
-
-  return isSuccess;
+  pimpl_->raw_redis_async_client.connect(
+      pimpl_->endpoint->address, pimpl_->endpoint->port, cb);
 }
 
+
+// TODO(inkeun): Finalize 를 추가한다.
+void InitializeRedisIoService() {
+  the_io_service = std::make_shared<boost::asio::io_service>();
+
+  // TODO(inkeun): 쓰레드 종료 과정을 추가로 확인하도록 만든다.
+  auto run_function = [the_io_service]() {
+    std::shared_ptr<boost::asio::io_service::work> work =
+        std::make_shared<boost::asio::io_service::work>(
+            *the_io_service);
+    the_io_service->run();
+  };
+
+  the_thread = std::make_shared<std::thread>(run_function);
 }
-}
+
+}  // namespace redis
+
+}  // namespace libgs
